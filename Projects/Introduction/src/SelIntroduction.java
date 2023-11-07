a4d9447ed338e7a92ec80e05729d6973e9fb056d
pp-8 <vishakha.p@prominentpixel.com>
2023-11-07 14:45:46 +0530

    Invoke Firefox Browser

--- Projects/Introduction/src/SelIntroduction.java
+++ Projects/Introduction/src/SelIntroduction.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class SelIntroduction {
@@ -7,8 +8,12 @@ public class SelIntroduction {
 	public static void main(String[] args) {
 		
 		// Invoking Chrome Browser
-		System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
+		//WebDriver driver = new ChromeDriver();
+		
+		// Invoking Firefox Browser - geckodriver
+		System.setProperty("webdriver.gecko.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/firefox_driver/geckodriver-v0.33.0-linux64/geckodriver");
+		WebDriver driver = new FirefoxDriver();
 
 	}
 
