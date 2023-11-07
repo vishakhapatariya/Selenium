1f5b190125519df92966676aa7ae8a823c81e70f
pp-8 <vishakha.p@prominentpixel.com>
2023-11-07 14:47:41 +0530

    Selenium WebDriver Methods

--- Projects/Introduction/src/SelIntroduction.java
+++ Projects/Introduction/src/SelIntroduction.java
@@ -8,12 +8,25 @@ public class SelIntroduction {
 	public static void main(String[] args) {
 		
 		// Invoking Chrome Browser
-		//System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
-		//WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
 		
 		// Invoking Firefox Browser - geckodriver
-		System.setProperty("webdriver.gecko.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/firefox_driver/geckodriver-v0.33.0-linux64/geckodriver");
-		WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.gecko.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/firefox_driver/geckodriver-v0.33.0-linux64/geckodriver");
+		//WebDriver driver = new FirefoxDriver();
+		
+		// Hit URL on browser
+		driver.get("https://www.youtube.com/");
+				
+		// Getting title of website
+		System.out.println(driver.getTitle());
+		
+		// Getting URL of current website
+		System.out.println(driver.getCurrentUrl());
+		
+		// Closing the website
+		driver.close();
+		// driver.quit();
 
 	}
 
