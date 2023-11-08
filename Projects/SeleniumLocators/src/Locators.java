f372ead7f729697b753090aa05021144f7ad9c3d
pp-8 <vishakha.p@prominentpixel.com>
2023-11-08 12:57:53 +0530

    LinkText and Xpath Locators

--- Projects/SeleniumLocators/src/Locators.java
+++ Projects/SeleniumLocators/src/Locators.java
@@ -11,7 +11,7 @@ public class Locators {
 		System.setProperty("webdriver.chrome.driver",
 				"/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		
+
 		// Resolve Synchronization Issue
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
@@ -23,8 +23,15 @@ public class Locators {
 		driver.findElement(By.name("inputPassword")).sendKeys("Hello");
 		// Click on Button using className Locator
 		driver.findElement(By.className("signInBtn")).click();
-		
+
 		// Extract text from Browser
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
+		// Click on the link using linkText Locator
+		driver.findElement(By.linkText("Forgot your password?")).click();
+
+		// Enter Name using Xpath Locator
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Vishakha");
+
 	}
 }
