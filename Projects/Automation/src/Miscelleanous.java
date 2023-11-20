0a768e8130ff3a58ffc38121d509fe4bb50723f3
pp-8 <vishakha.p@prominentpixel.com>
2023-11-20 12:16:32 +0530

    Take Screenshot in Selenium

--- Projects/Automation/src/Miscelleanous.java
+++ Projects/Automation/src/Miscelleanous.java
@@ -1,9 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Miscelleanous {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// Invoke Chrome Browser
 		System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
@@ -20,6 +25,11 @@ public class Miscelleanous {
 	
 		// Hit URL on Browser
 		driver.get("https://google.com/");
+		
+		// Take Screenshot
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File dest = new File("/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/Projects/Automation/src/screenshot.png");
+		FileUtils.copyFile(src,dest);
 		
 	}
 
