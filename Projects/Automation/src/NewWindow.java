213c73f5f950114d142f9154929776b290b1961a
pp-8 <vishakha.p@prominentpixel.com>
2023-11-21 13:06:05 +0530

    Taking WebElement Partial Screenshot

--- Projects/Automation/src/NewWindow.java
+++ Projects/Automation/src/NewWindow.java
@@ -1,13 +1,18 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// Invoke Chrome Browser
 		System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
@@ -33,13 +38,18 @@ public class NewWindow {
 		driver.get("https://rahulshettyacademy.com/");
 		
 		// Get text from second tab
-		String name = driver.findElement(By.xpath("//div[@id='courses-block']/div[1]//h2")).getText();
+		String courseName = driver.findElement(By.xpath("//div[@id='courses-block']/div[1]//h2")).getText();
 		
 		// Switch to first window
 		driver.switchTo().window(firstTabId);
 		
 		// Enter text
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(name);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		
+		// Taking WebElement Partial Screenshot
+		File src = name.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/Projects/Automation/src/partialScreenshot.png"));
 				
 		// Close all the tabs
 		driver.quit();
