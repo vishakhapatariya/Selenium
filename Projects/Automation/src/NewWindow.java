c612efc2a04268b645c303ef13559c04348c3ffd
pp-8 <vishakha.p@prominentpixel.com>
2023-11-21 12:43:24 +0530

    Invoking Multiple Windows & Tabs from Selenium

---
+++ Projects/Automation/src/NewWindow.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewWindow {
+
+	public static void main(String[] args) {
+
+		// Invoke Chrome Browser
+		System.setProperty("webdriver.chrome.driver", "/home/pp-8/Desktop/vishakha/Selenium_Training/Selenium/chrome_driver/chromedriver-linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+	
+		// Hit URL on Browser
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		
+		
+		// Open new Tab
+		driver.switchTo().newWindow(WindowType.TAB);
+		
+		// Get Id of all the tabs
+		Set<String> handles = driver.getWindowHandles();
+		Iterator<String> it = handles.iterator();
+		String firstTabId = it.next();
+		String secondTabId = it.next();
+		
+		// Switch to second window
+		driver.switchTo().window(secondTabId);
+		
+		// Hit URL on second Tab
+		driver.get("https://rahulshettyacademy.com/");
+		
+		// Get text from second tab
+		String name = driver.findElement(By.xpath("//div[@id='courses-block']/div[1]//h2")).getText();
+		
+		// Switch to first window
+		driver.switchTo().window(firstTabId);
+		
+		// Enter text
+		driver.findElement(By.cssSelector("[name='name']")).sendKeys(name);
+				
+		// Close all the tabs
+		driver.quit();
+	}
+
+}
