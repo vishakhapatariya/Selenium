9922544496d6adece9defe694c86353fe2e5a241
pp-8 <vishakha.p@prominentpixel.com>
2023-11-23 19:23:09 +0530

    Maven project and pom.xml dependencies

---
+++ Projects/MavenJava/src/main/java/MevenPractices/MavenJava/App.java
@@ -0,0 +1,13 @@
+package MevenPractices.MavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Projects/MavenJava/src/test/java/MevenPractices/MavenJava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package MevenPractices.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+
+	@Test
+	public void elementUI() {
+		System.out.println("elementUI");
+	}
+	
+}
