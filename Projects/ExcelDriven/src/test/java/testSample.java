bb250d2b25e91c3588d7e6e679089e909b149a1e
pp-8 <vishakha.p@prominentpixel.com>
2023-11-27 19:33:05 +0530

    Practice Exercise - Excel Driven testing

---
+++ Projects/ExcelDriven/src/test/java/testSample.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class testSample {
+
+	public static void main(String[] args) throws IOException {
+
+		dataDriven d = new dataDriven();
+		ArrayList data = d.getData("Add Profile");
+		
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+		
+	}
+
+}
