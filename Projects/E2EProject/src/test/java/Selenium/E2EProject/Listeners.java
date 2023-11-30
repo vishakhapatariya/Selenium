a9606f6ab99c9515dc7a7df939b131a38366dd78
pp-8 <vishakha.p@prominentpixel.com>
2023-11-30 17:01:45 +0530

    Extent reports Generation on Test Sequence Execution - E2E

--- Projects/E2EProject/src/test/java/Selenium/E2EProject/Listeners.java
+++ Projects/E2EProject/src/test/java/Selenium/E2EProject/Listeners.java
@@ -7,22 +7,32 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import resources.ExtentReporterNG;
 import resources.base;
 
 public class Listeners extends base implements ITestListener{
 
+	ExtentTest test;
+	ExtentReports extent = ExtentReporterNG.getReporterObject();
 	@Override
 	public void onTestStart(ITestResult result) {
-		// TODO Auto-generated method stub
+		test = extent.createTest(result.getMethod().getMethodName());
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		// TODO Auto-generated method stub
+		test.log(Status.PASS, "Test Passed");
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
+		
+		test.fail(result.getThrowable());
+		
 		WebDriver driver=null;
 		String testMethodName = result.getMethod().getMethodName();
 		try {
@@ -58,7 +68,7 @@ public class Listeners extends base implements ITestListener{
 
 	@Override
 	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
+		extent.flush();
 	}
 
 }
